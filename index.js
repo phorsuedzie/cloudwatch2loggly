675c3e5a2f14aca0a203d702d08e585f990c7e88
Tilo Prütz <tilo@infopark.de>
2017-03-01 18:45:50 +0100

    more like current lambda blueprint
    I’m not	sure what the benefit of callback over context.* is.

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ var logglyConfiguration = {
 // use KMS to decrypt customer token
 var decryptParams = {CiphertextBlob: new Buffer(process.env.kmsEncryptedCustomerToken, 'base64')};
 
-var kms = new AWS.KMS({
-    apiVersion: '2014-11-01'
-});
+var kms = new AWS.KMS({apiVersion: '2014-11-01'});
 
 kms.decrypt(decryptParams, function (error, data) {
     if (error) {
@@ -35,22 +33,7 @@ kms.decrypt(decryptParams, function (error, data) {
 });
 
 // entry point
-exports.handler = function (event, context) {
-    var payload = new Buffer(event.awslogs.data, 'base64');
-
-    zlib.gunzip(payload, function (error, result) {
-        if (error) {
-            context.fail(error);
-        } else {
-            var result_parsed = JSON.parse(result.toString('ascii'));
-            var parsedEvents = result_parsed.logEvents.map(function(logEvent) {
-                return parseEvent(logEvent, result_parsed.logGroup, result_parsed.logStream);
-            });
-
-            postEventsToLoggly(parsedEvents);
-        }
-    });
-
+exports.handler = function (event, context, callback) {
     // converts the event to a valid JSON object with the sufficient infomation required
     // - detects foreman prefixes and extracts it into own field
     // - detects and merges JSON data
@@ -80,7 +63,7 @@ exports.handler = function (event, context) {
         if (!logglyConfiguration.customerToken) {
             if (logglyConfiguration.tokenInitError) {
                 console.log('error in decrypt the token. Not retrying.');
-                return context.fail(logglyConfiguration.tokenInitError);
+                return callback(logglyConfiguration.tokenInitError);
             }
             console.log('Cannot flush logs since authentication token has not been initialized yet. Trying again in 100 ms.');
             setTimeout(function () { postEventsToLoggly(parsedEvents) }, 100);
@@ -90,13 +73,13 @@ exports.handler = function (event, context) {
         // get all the events, stringify them and join them
         // with the new line character which can be sent to Loggly
         // via bulk endpoint
-        var finalEvent = parsedEvents.map(JSON.stringify).join('\n');
+        const finalEvent = parsedEvents.map(JSON.stringify).join('\n');
 
         // creating logglyURL at runtime, so that user can change the tag or customer token in the go
         // by modifying the current script
         // create request options to send logs
         try {
-            var options = {
+            const options = {
                 hostname: logglyConfiguration.hostName,
                 path: '/bulk/' + logglyConfiguration.customerToken + '/tag/' + encodeURIComponent(logglyConfiguration.tags),
                 method: 'POST',
@@ -106,33 +89,47 @@ exports.handler = function (event, context) {
                 }
             };
 
-            var req = http.request(options, function (res) {
+            const req = http.request(options, function (res) {
                 res.on('data', function (result) {
                     result = JSON.parse(result.toString());
                     if (result.response === 'ok') {
-                        context.succeed('all events are sent to Loggly');
+                        callback(null, 'all events are sent to Loggly');
                     } else {
                         console.log(result.response);
                     }
                 });
                 res.on('end', function () {
                     console.log('No more data in response.');
-                    context.done();
+                    callback();
                 });
             });
 
             req.on('error', function (e) {
                 console.log('problem with request: ' + e.toString());
-                context.fail(e);
+                callback(e);
             });
 
             // write data to request body
             req.write(finalEvent);
             req.end();
-
         } catch (ex) {
             console.log(ex.message);
-            context.fail(ex.message);
+            callback(ex.message);
         }
     }
+
+    const payload = new Buffer(event.awslogs.data, 'base64');
+
+    zlib.gunzip(payload, function (error, result) {
+        if (error) {
+            callback(error);
+        } else {
+            var result_parsed = JSON.parse(result.toString('ascii'));
+            var parsedEvents = result_parsed.logEvents.map(function(logEvent) {
+                return parseEvent(logEvent, result_parsed.logGroup, result_parsed.logStream);
+            });
+
+            postEventsToLoggly(parsedEvents);
+        }
+    });
 };
