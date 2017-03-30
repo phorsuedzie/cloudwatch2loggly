6134dab26eb542f48f0acbecc4b420b3400c2214
Tilo Prütz <tilo@infopark.de>
2017-03-30 19:12:33 +0200

    test for event parsing

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -5,24 +5,30 @@ const Aws = require('aws-sdk');
 Aws.config.update({region: 'eu-west-1'});
 const zlib = require('zlib');
 const http = require('http');
+const LogEventParser = require('scrivito-log-event-parser');
 
 describe("cloudwatch2loggly", () => {
   var CloudWatch2Loggly;
 
   var event;
   var handleEvent = () => { return LambdaTester(CloudWatch2Loggly.handler).event(event); };
+  var buildEvent = (e) => {
+    return {"awslogs": {"data": zlib.gzipSync(JSON.stringify(e)).toString("base64")}};
+  };
+
   beforeEach(() => {
-    event = {
-      "awslogs": {
-        "data": zlib.gzipSync('{"logEvents":[]}').toString("base64")
-      }
-    };
+    process.env.kmsEncryptedCustomerToken = "Zm9v";
+
+    event = buildEvent({
+      logEvents: ["event 1", "event 2", "event 3"],
+      logGroup: "log group",
+      logStream: "log stream",
+    });
     this.decryptResult = {Plaintext: "token"};
     this.kms = {decrypt: (params, callback) => { callback(null, this.decryptResult); }};
     this.kmsSpy = spyOn(Aws, 'KMS').and.returnValue(this.kms);
 
-    // tempoarily, to complete functional tests before refactoring
-    process.env.kmsEncryptedCustomerToken = "Zm9v";
+    this.parseSpy = spyOn(LogEventParser, 'parse').and.returnValue("parsed event");
 
     spyOn(http, 'request').and.callFake((options, responseHandler) => {
       var res = {on: (key, callback) => { res[key] = callback; }};
@@ -40,6 +46,14 @@ describe("cloudwatch2loggly", () => {
     CloudWatch2Loggly = require('../index');
     handleEvent().expectResult(() => {
       expect(decryptSpy).toHaveBeenCalled();
+    }).verify(done);
+  });
+
+  it("parses the events using the LogEventParser", (done) => {
+    handleEvent().expectResult(() => {
+      expect(this.parseSpy).toHaveBeenCalledWith("event 1", "log group", "log stream");
+      expect(this.parseSpy).toHaveBeenCalledWith("event 2", "log group", "log stream");
+      expect(this.parseSpy).toHaveBeenCalledWith("event 3", "log group", "log stream");
     }).verify(done);
   });
 });
