228ed453e6b4d6c664133e89343b24a19d79b3e4
Tilo Prütz <tilo@infopark.de>
2017-04-05 08:49:56 +0200

    [spec] prepared testing of other event sources

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -9,29 +9,17 @@ const LogEventParser = require('scrivito-log-event-parser');
 
 describe("cloudwatch2loggly", () => {
   var CloudWatch2Loggly = require('../index');
-
   var event;
   var handleEvent = () => { return LambdaTester(CloudWatch2Loggly.handler).event(event); };
-  var buildEvent = (e) => {
-    return {"awslogs": {"data": zlib.gzipSync(JSON.stringify(e)).toString("base64")}};
-  };
 
   beforeEach(() => {
     process.env.kmsEncryptedCustomerToken = "Zm9v";
     process.env.logglyHostName = "the loggly host";
     process.env.logglyTags = "the loggly tags";
 
-    event = buildEvent({
-      logEvents: ["event 1", "event 2", "event 3"],
-      logGroup: "log group",
-      logStream: "log stream",
-    });
-
     this.decryptResult = {promise: () => { return Promise.resolve({Plaintext: "token"}); }};
     this.kms = {decrypt: () => { return this.decryptResult; }};
     this.kmsSpy = spyOn(Aws, 'KMS').and.returnValue(this.kms);
-
-    this.parseSpy = spyOn(LogEventParser, 'parse').and.returnValue("parsed event");
 
     this.request = {
       on: () => {},
@@ -49,68 +37,34 @@ describe("cloudwatch2loggly", () => {
     this.logSpy = spyOn(console, 'log');
   });
 
-  it("decrypts the Loggly API token", (done) => {
-    var decryptSpy = spyOn(this.kms, 'decrypt').and.returnValue(this.decryptResult);
-    handleEvent().expectResult(() => {
-      expect(decryptSpy).toHaveBeenCalledWith({CiphertextBlob: new Buffer("Zm9v", 'base64')});
-    }).verify(done);
-  });
+  describe("with CloudWatchLogs source", () => {
+    var buildEvent = (e) => {
+      return {"awslogs": {"data": zlib.gzipSync(JSON.stringify(e)).toString("base64")}};
+    };
 
-  it("parses the events using the LogEventParser", (done) => {
-    handleEvent().expectResult(() => {
-      expect(this.parseSpy).toHaveBeenCalledWith("event 1", "log group", "log stream");
-      expect(this.parseSpy).toHaveBeenCalledWith("event 2", "log group", "log stream");
-      expect(this.parseSpy).toHaveBeenCalledWith("event 3", "log group", "log stream");
-    }).verify(done);
-  });
-
-  it("transfers the events to Loggly", (done) => {
-    var requestWriteSpy = spyOn(this.request, 'write');
-    var requestEndSpy = spyOn(this.request, 'end').and.callThrough();
-    handleEvent().expectResult(() => {
-      expect(this.requestSpy).toHaveBeenCalled();
-      var requestOptions = this.requestSpy.calls.argsFor(0)[0];
-      expect(requestOptions.hostname).toEqual("the loggly host");
-      expect(requestOptions.path).toEqual('/bulk/token/tag/the%20loggly%20tags');
-      expect(requestOptions.method).toEqual('POST');
-      expect(requestOptions.headers)
-          .toEqual({"Content-Type": 'application/json', "Content-Length": 44});
-
-      expect(requestWriteSpy)
-          .toHaveBeenCalledWith('"parsed event"\n"parsed event"\n"parsed event"');
-
-      expect(requestEndSpy).toHaveBeenCalled();
-    }).verify(done);
-  });
-
-  it("logs the Loggly response", (done) => {
-    this.requestSpy.and.callFake((options, responseHandler) => {
-      var res = {on: (key, callback) => { res[key] = callback; }};
-      res.statusCode = 200;
-      responseHandler(res);
-      this.request.onEnd = () => {
-        res.data("part one");
-        res.data("part two");
-        res.end();
-      };
-      return this.request;
-    });
-
-    handleEvent().expectResult(() => {
-      expect(this.logSpy).toHaveBeenCalledWith("Loggly response status code: 200");
-      expect(this.logSpy).toHaveBeenCalledWith("Loggly responded: part one");
-      expect(this.logSpy).toHaveBeenCalledWith("Loggly responded: part two");
-    }).verify(done);
-  });
-
-  describe("when log event contains UTF-8", () => {
     beforeEach(() => {
-      this.parseSpy.and.callFake((input) => { return `parsed ${input}`; });
       event = buildEvent({
-        logEvents: ["€vent", "üvent", "evenд"],
+        logEvents: ["event 1", "event 2", "event 3"],
         logGroup: "log group",
         logStream: "log stream",
       });
+
+      this.parseSpy = spyOn(LogEventParser, 'parse').and.returnValue("parsed event");
+    });
+
+    it("decrypts the Loggly API token", (done) => {
+      var decryptSpy = spyOn(this.kms, 'decrypt').and.returnValue(this.decryptResult);
+      handleEvent().expectResult(() => {
+        expect(decryptSpy).toHaveBeenCalledWith({CiphertextBlob: new Buffer("Zm9v", 'base64')});
+      }).verify(done);
+    });
+
+    it("parses the events using the LogEventParser", (done) => {
+      handleEvent().expectResult(() => {
+        expect(this.parseSpy).toHaveBeenCalledWith("event 1", "log group", "log stream");
+        expect(this.parseSpy).toHaveBeenCalledWith("event 2", "log group", "log stream");
+        expect(this.parseSpy).toHaveBeenCalledWith("event 3", "log group", "log stream");
+      }).verify(done);
     });
 
     it("transfers the events to Loggly", (done) => {
@@ -118,87 +72,137 @@ describe("cloudwatch2loggly", () => {
       var requestEndSpy = spyOn(this.request, 'end').and.callThrough();
       handleEvent().expectResult(() => {
         expect(this.requestSpy).toHaveBeenCalled();
-        expect(requestWriteSpy).
-            toHaveBeenCalledWith('"parsed €vent"\n"parsed üvent"\n"parsed evenд"');
+        var requestOptions = this.requestSpy.calls.argsFor(0)[0];
+        expect(requestOptions.hostname).toEqual("the loggly host");
+        expect(requestOptions.path).toEqual('/bulk/token/tag/the%20loggly%20tags');
+        expect(requestOptions.method).toEqual('POST');
+        expect(requestOptions.headers)
+            .toEqual({"Content-Type": 'application/json', "Content-Length": 44});
+
+        expect(requestWriteSpy)
+            .toHaveBeenCalledWith('"parsed event"\n"parsed event"\n"parsed event"');
+
         expect(requestEndSpy).toHaveBeenCalled();
       }).verify(done);
     });
-  });
 
-  // This sounds wrong but is what has been observed: Loggly returns an OK result but the response's
-  // “end” callback is not being called within the lambda's timeout. The request timeout isn't
-  // triggered either.
-  describe("when HTTPS response is not ended properly", () => {
-    beforeEach(() => {
+    it("logs the Loggly response", (done) => {
       this.requestSpy.and.callFake((options, responseHandler) => {
         var res = {on: (key, callback) => { res[key] = callback; }};
+        res.statusCode = 200;
         responseHandler(res);
-        this.request.onEnd = () => { res.data('{"response": "ok"}'); };
+        this.request.onEnd = () => {
+          res.data("part one");
+          res.data("part two");
+          res.end();
+        };
         return this.request;
+      });
+
+      handleEvent().expectResult(() => {
+        expect(this.logSpy).toHaveBeenCalledWith("Loggly response status code: 200");
+        expect(this.logSpy).toHaveBeenCalledWith("Loggly responded: part one");
+        expect(this.logSpy).toHaveBeenCalledWith("Loggly responded: part two");
+      }).verify(done);
+    });
+
+    describe("when log event contains UTF-8", () => {
+      beforeEach(() => {
+        this.parseSpy.and.callFake((input) => { return `parsed ${input}`; });
+        event = buildEvent({
+          logEvents: ["€vent", "üvent", "evenд"],
+          logGroup: "log group",
+          logStream: "log stream",
+        });
+      });
+
+      it("transfers the events to Loggly", (done) => {
+        var requestWriteSpy = spyOn(this.request, 'write');
+        var requestEndSpy = spyOn(this.request, 'end').and.callThrough();
+        handleEvent().expectResult(() => {
+          expect(this.requestSpy).toHaveBeenCalled();
+          expect(requestWriteSpy).
+              toHaveBeenCalledWith('"parsed €vent"\n"parsed üvent"\n"parsed evenд"');
+          expect(requestEndSpy).toHaveBeenCalled();
+        }).verify(done);
       });
     });
 
-    it("succeeds nevertheless", (done) => { handleEvent().expectResult().verify(done); });
-
-    describe("when a response chunk is not JSON", () => {
+    // This sounds wrong but is what has been observed: Loggly returns an OK result but the
+    // response's “end” callback is not being called within the lambda's timeout. The request
+    // timeout isn't triggered either.
+    describe("when HTTPS response is not ended properly", () => {
       beforeEach(() => {
         this.requestSpy.and.callFake((options, responseHandler) => {
           var res = {on: (key, callback) => { res[key] = callback; }};
           responseHandler(res);
-          this.request.onEnd = () => {
-            res.data("this ain't JSON");
-            res.data('{"response": "ok"}');
-          };
+          this.request.onEnd = () => { res.data('{"response": "ok"}'); };
           return this.request;
         });
       });
 
       it("succeeds nevertheless", (done) => { handleEvent().expectResult().verify(done); });
-    });
-  });
 
-  describe("when JSON parsing fails", () => {
-    beforeEach(() => {
-      event = {
-        "awslogs": {
-          "data": zlib.gzipSync("this ain't json").toString("base64")
-        }
-      };
-    });
+      describe("when a response chunk is not JSON", () => {
+        beforeEach(() => {
+          this.requestSpy.and.callFake((options, responseHandler) => {
+            var res = {on: (key, callback) => { res[key] = callback; }};
+            responseHandler(res);
+            this.request.onEnd = () => {
+              res.data("this ain't JSON");
+              res.data('{"response": "ok"}');
+            };
+            return this.request;
+          });
+        });
 
-    it("logs details and fails with the occurred error", (done) => {
-      handleEvent().expectError((error) => {
-        expect(error).toEqual(SyntaxError('Unexpected token h in JSON at position 1'));
-        expect(this.logSpy)
-            .toHaveBeenCalledWith("Error while parsing json. Input: this ain't json");
-      }).verify(done);
-    });
-  });
-
-  describe("when token decryption fails", () => {
-    beforeEach(() => { stubAwsServiceCall(this.kms, 'decrypt').setReturnValue(Error('fail')); });
-
-    it("fails with the occurred error", (done) => {
-      handleEvent().expectError((error) => {
-        expect(error).toEqual(Error("fail"));
-      }).verify(done);
-    });
-  });
-
-  describe("when loggly responds with error", () => {
-    beforeEach(() => {
-      this.requestSpy.and.callFake((options, responseHandler) => {
-        var res = {on: (key, callback) => { res[key] = callback; }};
-        responseHandler(res);
-        this.request.onEnd = () => { throw "failure"; };
-        return this.request;
+        it("succeeds nevertheless", (done) => { handleEvent().expectResult().verify(done); });
       });
     });
 
-    it("fails with the occurred error", (done) => {
-      handleEvent().expectError((error) => {
-        expect(error).toEqual(Error("failure"));
-      }).verify(done);
+    describe("when JSON parsing fails", () => {
+      beforeEach(() => {
+        event = {
+          "awslogs": {
+            "data": zlib.gzipSync("this ain't json").toString("base64")
+          }
+        };
+      });
+
+      it("logs details and fails with the occurred error", (done) => {
+        handleEvent().expectError((error) => {
+          expect(error).toEqual(SyntaxError('Unexpected token h in JSON at position 1'));
+          expect(this.logSpy)
+              .toHaveBeenCalledWith("Error while parsing json. Input: this ain't json");
+        }).verify(done);
+      });
+    });
+
+    describe("when token decryption fails", () => {
+      beforeEach(() => { stubAwsServiceCall(this.kms, 'decrypt').setReturnValue(Error('fail')); });
+
+      it("fails with the occurred error", (done) => {
+        handleEvent().expectError((error) => {
+          expect(error).toEqual(Error("fail"));
+        }).verify(done);
+      });
+    });
+
+    describe("when loggly responds with error", () => {
+      beforeEach(() => {
+        this.requestSpy.and.callFake((options, responseHandler) => {
+          var res = {on: (key, callback) => { res[key] = callback; }};
+          responseHandler(res);
+          this.request.onEnd = () => { throw "failure"; };
+          return this.request;
+        });
+      });
+
+      it("fails with the occurred error", (done) => {
+        handleEvent().expectError((error) => {
+          expect(error).toEqual(Error("failure"));
+        }).verify(done);
+      });
     });
   });
 });
